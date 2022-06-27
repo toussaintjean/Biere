885e30fe8cdca4229ef76139c8705b18a0caee30
toussaintjean <j.toussaint@epmistes.net>
2022-06-27 14:30:36 +0200

    commit j1 matin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { GroupeComponent } from './groupe/groupe.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { PreferenceComponent } from './preference/preference.component';
+import { RechercheComponent } from './recherche/recherche.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'connexion',component: ConnexionComponent},
+  {path:'preference',component: PreferenceComponent},
+  {path:'groupe',component: GroupeComponent},
+  {path:'recherche',component: RechercheComponent},
+  {path:'inscription',component: InscriptionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { PreferenceComponent } from './preference/preference.component';
+import { GroupeComponent } from './groupe/groupe.component';
+import { RechercheComponent } from './recherche/recherche.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent,
+    PreferenceComponent,
+    GroupeComponent,
+    RechercheComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule,
