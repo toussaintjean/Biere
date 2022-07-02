1844252ec22ca1e326ec106c0d82f8324bc4733f
vingolfxd <biouclem@gmail.com>
2022-07-02 17:39:22 +0200

    Clement0207findejournee

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,15 +7,17 @@ import { PreferenceComponent } from './preference/preference.component';
 import { RechercheComponent } from './recherche/recherche.component';
 import { ConnexionbisComponent } from './connexionbis/connexionbis.component';
 import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: 'home', component: HomeComponent},
   {path: 'connexionbis', component: ConnexionbisComponent},
   {path:'preference',component: PreferenceComponent},
   {path:'groupe',component: GroupeComponent},
   {path:'recherche',component: RechercheComponent},
   {path:'inscription',component: InscriptionComponent},
  // {path:'barnav',component: BarnavComponent},
-  {path:'',redirectTo: "connexionbis",  pathMatch: 'full'},
+  {path:'**',redirectTo: "home",  pathMatch: 'full'},
   {path: 'test', component:TestComponent}
 ];
 
--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -20,6 +20,7 @@ export class InscriptionComponent implements OnInit {
     this.http.post("http://localhost:8086/user", user).subscribe({
       next: (data) => {
         console.log(data)
+       // SendMailInscription();
         this.message = "Vous êtes désormais inscrit ! <br/>Veuillez patientez, vous allez être redirigé vers la page de connexion dans 5 secondes..."
         setTimeout(() => {
           this.route.navigateByUrl('connexionbis')
@@ -28,4 +29,54 @@ export class InscriptionComponent implements OnInit {
     error: (err) => {console.log(err)}
   })
 }
+
+// sendMailInscirption(){
+//   final String username = "biouclem@gmail.com";
+// final String password = "tmbulgrapbykbsoi";
+
+
+
+// Properties props = new Properties();
+// props.put("mail.smtp.auth", "true");
+// props.put("mail.smtp.starttls.enable", "true");
+// props.put("mail.smtp.host", "smtp.gmail.com");
+// props.put("mail.smtp.port", "587");
+// props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+// props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+
+
+
+// Session session = Session.getInstance(props,
+// new javax.mail.Authenticator() {
+// protected PasswordAuthentication getPasswordAuthentication() {
+// return new PasswordAuthentication(username, password);
+// }
+// });
+
+
+
+// try {
+
+
+
+// Message message = new MimeMessage(session);
+// message.setFrom(new InternetAddress("biouclem@gmail.com"));
+// message.setRecipients(Message.RecipientType.TO,
+// InternetAddress.parse(to));
+// message.setSubject("Convocation Evaluation");
+// message.setText(msg);
+// Transport.send(message);
+
+
+// }
+
+
+
+// catch (MessagingException e)
+// {
+// // throw new RuntimeException(e);
+// System.out.println(e.getMessage());
+// }
+// }
 }
+
